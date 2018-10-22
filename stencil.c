a4f1b233848eba897e6cfb21db7a6e15017a3578
Unknown <fj16315@my.bristol.ac.uk>
2018-10-22 22:28:56 +0100

    changed checker board datatype from char to float

--- stencil.c
+++ stencil.c
@@ -6,9 +6,9 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const int nx, const int ny, char *  image, char *  tmp_image);
-void init_image(const int nx, const int ny, char *  image, char *  tmp_image);
-void output_image(const char * file_name, const int nx, const int ny, char *image);
+void stencil(const int nx, const int ny, float *  image, float *  tmp_image);
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image);
+void output_image(const char * file_name, const int nx, const int ny, float *image);
 double wtime(void);
 
 int main(int argc, char *argv[]) {
@@ -25,8 +25,8 @@ int main(int argc, char *argv[]) {
   int niters = atoi(argv[3]);
 
   // Allocate the image
-  char *image = malloc(sizeof(char)*nx*ny);
-  char *tmp_image = malloc(sizeof(char)*nx*ny);
+  float *image = malloc(sizeof(float)*nx*ny);
+  float *tmp_image = malloc(sizeof(float)*nx*ny);
 
   // Set the input image
   init_image(nx, ny, image, tmp_image);
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
   free(image);
 }
 
-void stencil(const int nx, const int ny, char *  /*restrict*/ image, char *  /*restrict*/ tmp_image) {
+void stencil(const int nx, const int ny, float *  /*restrict*/ image, float *  /*restrict*/ tmp_image) {
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) { //Image stored as arrayof Doubles, column by column
       tmp_image[j+i*ny] = image[j+i*ny] * 0.6; //Weight current pixel
@@ -62,7 +62,7 @@ void stencil(const int nx, const int ny, char *  /*restrict*/ image, char *  /*r
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, char *  image, char *  tmp_image) {
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image) {
   // Zero everything
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
@@ -88,7 +88,7 @@ void init_image(const int nx, const int ny, char *  image, char *  tmp_image) {
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char * file_name, const int nx, const int ny, char *image) {
+void output_image(const char * file_name, const int nx, const int ny, float *image) {
 
   // Open output file
   FILE *fp = fopen(file_name, "w");
