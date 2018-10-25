847a1364392f01387b6aa687c7072fb2e77e1668
Unknown <fj16315@my.bristol.ac.uk>
2018-10-25 22:03:50 +0100

    removed if statements with hard coding edges and corners

--- stencil.c
+++ stencil.c
@@ -50,14 +50,39 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, float *  restrict image, float *  restrict tmp_image) {
-  for (int i = 0; i < nx; ++i) {
-    for (int j = 0; j < ny; ++j) { //Image stored as arrayof Doubles, column by column
+  for (int i = 1; i < nx-1; i++) {
+    tmp_image[i] = image[i] * 0.6f; //Weight current pixel
+    tmp_image[i] += (image[i-1] + image[i+1] + image[i+nx]) * 0.1f;
+    tmp_image[((ny-1)*nx)+i] = image[((ny-1)*nx)+i] * 0.6f; //Weight current pixel
+    tmp_image[((ny-1)*nx)+i] += (image[((ny-1)*nx)+i-1] + image[((ny-1)*nx)+i+1] + image[((ny-2)*nx)+i]) * 0.1f;
+  }
+
+  for (int j = 1; j < ny-1; j++) {
+    tmp_image[j*ny] = image[j*ny] * 0.6f; //Weight current pixel
+    tmp_image[j*ny] += (image[(j-1)*ny] + image[(j+1)*ny] + image[(j*ny)+1]) * 0.1f;
+    tmp_image[((j+1)*ny)-1] = image[((j+1)*ny)-1] * 0.6f;
+    tmp_image[((j+1)*ny)-1] += (image[(j*ny)-1] + image[((j+2)*ny)-1] + image[((j+1)*ny)-2]) * 0.1f;
+  }
+
+  tmp_image[0] = image[0] * 0.6f;
+  tmp_image[0] += (image[1] + image[nx]) * 0.1f;
+  tmp_image[nx-1] = image[nx-1] * 0.6f;
+  tmp_image[nx-1] += (image[nx-2] + image[(2*nx)-1]) * 0.1f;
+  tmp_image[(ny-1)*nx] = image[(ny-1)*nx] * 0.6f;
+  tmp_image[(ny-1)*nx] += (image[(ny-2)*nx] + image[((ny-1)*nx)+1]) * 0.1f;
+  tmp_image[(ny*nx)-1] = image[(ny*nx)-1] * 0.6f;
+  tmp_image[(ny*nx)-1] += (image[((ny-1)*nx)-1] + image[(ny*nx)-2]) * 0.1f;
+
+
+
+  for (int i = 1; i < nx-1; ++i) {
+    for (int j = 1; j < ny-1; ++j) { //Image stored as arrayof Doubles, column by column
       tmp_image[j+i*ny] = image[j+i*ny] * 0.6f; //Weight current pixel
       /*if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1f; //Above current pixel
       if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.1f; //Below current pixel
       if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.1f; //Left of current pixel
       if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.1f; //Right of current pixel*/
-      //tmp_image[j+i*(ny+2)] += (image[j  +(i-1)*(ny+2)] + image[j  +(i+1)*(ny+2)] + image[j-1+i*(ny+2)] + image[j+1+i*(ny+2)]) * 0.1f;
+      tmp_image[j+i*ny] += (image[j  +(i-1)*ny] + image[j  +(i+1)*ny] + image[j-1+i*ny] + image[j+1+i*ny]) * 0.1f;
     }
   }
 }
